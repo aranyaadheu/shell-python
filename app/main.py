b8dac42b9dd61156238fb0d64ce67a95782c2fd1
Aniruddha Aranya A Dheu <aranyaaniruddha@gmail.com>
2025-06-05 17:18:05 +0600

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,10 +4,9 @@ import sys
 def main():
     sys.stdout.write("$ ")
 
-    # Wait for user input
     command = input()
     print(f"{command}: command not found")
+    main()
 
 
 if __name__ == "__main__":
-    main()
